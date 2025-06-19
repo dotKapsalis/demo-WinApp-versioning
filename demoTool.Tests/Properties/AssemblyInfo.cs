9873cb362ba9b7df305cac05579159ebc16c3ff1
Kostas Kapsalis <kapsalisk.develop@hotmail.com>
2025-06-20 01:38:58 +0300

    refactor workflows and added unit tests

---
+++ demoTool.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("demoTool.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("demoTool.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("52c212a6-2823-4f22-b92b-d1ec2e832d1d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ demoTool.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using demoTool.Desktop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace demoTool.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_ShouldReturnSum_WhenTwoIntegersProvided()
+        {
+            Calculator calculator = new Calculator();
+
+            int result = calculator.AddNumbers(2, 3);
+
+            Assert.AreEqual(5, result);
+        }
+    }
+}
