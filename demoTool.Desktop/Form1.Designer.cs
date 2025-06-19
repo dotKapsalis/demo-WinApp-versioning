0ba0ad9eed259dadfff48e2fd7207a539231a8f2
Kostas Kapsalis <kapsalisk.develop@hotmail.com>
2025-06-19 04:19:56 +0300

    move control

--- demoTool.Desktop/Form1.Designer.cs
+++ demoTool.Desktop/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(12, 138);
+            this.checkBox1.Location = new System.Drawing.Point(74, 131);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(80, 17);
             this.checkBox1.TabIndex = 0;
