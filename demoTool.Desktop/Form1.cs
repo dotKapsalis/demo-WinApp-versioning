f9a3c82168228d1f84376cd13dea80c4255a05b4
Kostas Kapsalis <kapsalisk.develop@hotmail.com>
2025-06-23 20:54:27 +0300

    added another test for button

--- demoTool.Desktop/Form1.cs
+++ demoTool.Desktop/Form1.cs
@@ -15,6 +15,13 @@ namespace demoTool.Desktop
         public Form1()
         {
             InitializeComponent();
+
+            button1.Click += Button1_Click;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
--- demoTool.Tests/UnitTest1.cs
+++ demoTool.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using demoTool.Desktop;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace demoTool.Tests
 {
@@ -15,6 +16,19 @@ namespace demoTool.Tests
             int result = calculator.AddNumbers(2, 3);
 
             Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Form1_HasButton1_WithCorrectDefaults()
+        {
+            using (var form = new Form1())
+            {
+                var button = form.Controls.Find("button1", true).FirstOrDefault() as Button;
+
+                Assert.IsNotNull(button);
+                Assert.AreEqual("Start", button.Text);
+                Assert.IsTrue(button.Enabled);
+            }
         }
     }
 }
